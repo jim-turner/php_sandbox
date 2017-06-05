4db6631f1f54ca4c217ba36e32c09a3fc0bc8741
Jim Turner <jimturnerstudio@gmail.com>
2017-06-05 15:13:43 -0600

    June 5 Second Commit

---
+++ php_projects/includes.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+
+    <title>Includes</title>
+
+</head>
+
+<body>
+
+<?php
+    include("included_func.php");
+    ?>
+    
+    <?php hello("Everyone"); ?>
+
+</body>
+
+
+
+
+</html>
