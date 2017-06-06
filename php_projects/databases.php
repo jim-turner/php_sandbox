6df3dff35ace8b639b87ece12c91477a9554f557
Jim Turner <jimturnerstudio@gmail.com>
2017-06-05 19:06:35 -0600

    June5 Last Commit - with any luck

---
+++ php_projects/databases.php
@@ -0,0 +1,28 @@
+<?php
+
+//1. Create a database connection
+
+$connection = mysqli_connect("localhost", "root", "root");
+if(!$connection){
+    die("Database connection failed:" . mysql_error());
+}
+?>
+
+
+<html>
+
+<head>
+
+    <title>Databases</title>
+
+</head>
+
+<body>
+
+
+</body>
+
+
+
+
+</html>
