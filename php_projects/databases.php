08c8742393dc90d03e8e1c1159cee5d6bc539221
Jim Turner <jimturnerstudio@gmail.com>
2017-06-06 07:51:16 -0600

    First commint for May6 2017

--- php_projects/databases.php
+++ php_projects/databases.php
@@ -2,10 +2,14 @@
 
 //1. Create a database connection
 
-$connection = mysqli_connect("localhost", "root", "root");
+$connection = mysqli_connect("localhost", "root", "root", "widget_corp");
 if(!$connection){
-    die("Database connection failed:" . mysql_error());
+    die("Database connection failed:" . mysqli_error());
 }
+
+
+    
+
 ?>
 
 
@@ -18,11 +22,25 @@ if(!$connection){
 </head>
 
 <body>
-
-
+<?php
+$result = mysqli_query($connection, "SELECT * FROM subjects" );
+    if (!$result){
+    die("Database query failed:" . mysql_error());
+    }
+    
+    //results
+    while($row = mysqli_fetch_array($result)){
+        echo $row["menu_name"]. " " .$row["position"]."<br />";
+    }
+    ?>
 </body>
 
-
-
-
 </html>
+
+
+<?php
+//close connection
+
+mysqli_close($connection);
+    
+    ?>
